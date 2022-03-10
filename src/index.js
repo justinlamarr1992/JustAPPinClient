5b610557349d32b6ac187c03a65940c4cd156194
Justin Williams <justinlamarr1992@icloud.com>
2022-03-10 10:32:03 -0600

    STASHING

--- src/index.js
+++ src/index.js
@@ -16,16 +16,6 @@ import reportWebVitals from "./reportWebVitals";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
-// ReactDOM.render(
-//   // <React.StrictMode>
-//   <Provider store={store}>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </Provider>,
-//   // </React.StrictMode>
-//   document.getElementById("root")
-// );
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -35,7 +25,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
