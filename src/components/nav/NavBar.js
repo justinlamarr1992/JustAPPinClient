d662593fa187252cc74136592e57cf7a0da3bdc7
Justin <justinlamarr1992@icloud.com>
2022-03-10 10:25:32 -0600

    Pushed to firbase hostion

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -44,75 +44,141 @@ const NavBar = () => {
   };
   return (
     // find out if there is a div here
-    <Menu
-      onClick={handleClick}
-      selectedKeys={[current]}
-      mode="horizontal"
-      className=""
-      // style={{
-      //   backgroundColor: "00a3df",
-      //   color: "#00a3df",
-      // }}
-    >
-      <Item>
-        <Image className="float-start" preview={false} width={75} src={Logo} />
-      </Item>
-      <Item key="home" icon={<HomeOutlined />}>
-        <Link to="/">Home</Link>
-      </Item>
+    <header class="" id="myNavBar">
+      <div class="header-container navbar">
+        <a href="#hero" class="active logo">
+          <Link to="/">
+            <Image
+              className="float-start"
+              preview={false}
+              width={75}
+              src={Logo}
+            />
+          </Link>
+        </a>
+        <a href="#" class="toggle-button">
+          <i class="fa fa-bars fa-2x"></i>
+        </a>
+        <nav class="navbar-links">
+          <ul class="nav-links">
+            <li>
+              <Link to="/store">E-Commerce Home</Link>
+            </li>
+            <li>
+              <Link to="/shop">Products Page</Link>
+            </li>
+            <li>
+              <Link to="/cart">
+                <Badge count={cart.length} offset={[9, 0]}>
+                  Shopping Cart
+                </Badge>
+              </Link>
+            </li>
+            <li>
+              {!user && (
+                <Link key="login" className=" float-end" to="/login">
+                  Login
+                </Link>
+              )}
+            </li>
+            <li>
+              {!user && (
+                <Link key="register" className=" float-end" to="/register">
+                  Register
+                </Link>
+              )}
+            </li>
+            <li>
+              {user && user.role === "lead" && (
+                <Item>
+                  <Link to="/user/history">Dashboard</Link>
+                </Item>
+              )}
+              {user && user.role === "admin" && (
+                <Item>
+                  <Link to="/admin/dashboard">Dashboard</Link>
+                </Item>
+              )}
+            </li>
+            <li>
+              <h3 onClick={logout}>LogOut</h3>
+            </li>
+          </ul>
+        </nav>
+        <a class="header-cta-button" href="#cta">
+          <button>Contact Us</button>
+        </a>
+      </div>
+    </header>
+    // <Menu
+    //   onClick={handleClick}
+    //   selectedKeys={[current]}
+    //   mode="horizontal"
+    //   className=""
+    //   // style={{
+    //   //   backgroundColor: "00a3df",
+    //   //   color: "#00a3df",
+    //   // }}
+    // >
+    //   <Item>
+    //     <Image className="float-start" preview={false} width={75} src={Logo} />
+    //   </Item>
+    //   <Item key="home" icon={<HomeOutlined />}>
+    //     <Link to="/">Home</Link>
+    //   </Item>
 
-      <Item key="store" icon={<SkinOutlined />}>
-        <Link to="/store">Store Page</Link>
-      </Item>
-      <Item key="shop" icon={<ShoppingOutlined />}>
-        <Link to="/shop">Shop</Link>
-      </Item>
-      <Item key="cart" icon={<ShoppingCartOutlined />}>
-        <Link to="/cart">
-          <Badge count={cart.length} offset={[9, 0]}>
-            Cart
-          </Badge>
-        </Link>
-      </Item>
+    //   <Item key="store" icon={<SkinOutlined />}>
+    //     <Link to="/store">Store Page</Link>
+    //   </Item>
+    //   <Item key="shop" icon={<ShoppingOutlined />}>
+    //     <Link to="/shop">Shop</Link>
+    //   </Item>
+    //   <Item key="cart" icon={<ShoppingCartOutlined />}>
+    //     <Link to="/cart">
+    //       <Badge count={cart.length} offset={[9, 0]}>
+    //         Cart
+    //       </Badge>
+    //     </Link>
+    //   </Item>
 
-      {!user && (
-        <Item key="register" className=" float-end" icon={<UserAddOutlined />}>
-          <Link to="/register">Register</Link>
-        </Item>
-      )}
+    //   {!user && (
+    //     <Item key="register" className=" float-end" icon={<UserAddOutlined />}>
+    //       <Link to="/register">Register</Link>
+    //     </Item>
+    //   )}
 
-      {!user && (
-        <Item key="login" icon={<UserOutlined />} className=" float-end">
-          <Link to="/login">Login</Link>
-        </Item>
-      )}
+    //   {!user && (
+    //     <Item key="login" icon={<UserOutlined />} className=" float-end">
+    //       <Link to="/login">Login</Link>
+    //     </Item>
+    //   )}
 
-      {user && (
-        <SubMenu
-          key="sub-menu"
-          icon={<SettingOutlined />}
-          // title={user.email && user.email.split("@")[0]}
-          title={user.email && user.email.split("@")[0]}
-        >
-          {user && user.role === "lead" && (
-            <Item>
-              <Link to="/user/history">Dashboard</Link>
-            </Item>
-          )}
-          {user && user.role === "admin" && (
-            <Item>
-              <Link to="/admin/dashboard">Dashboard</Link>
-            </Item>
-          )}
-          <Item icon={<LogoutOutlined />} onClick={logout}>
-            Logout
-          </Item>
-        </SubMenu>
-      )}
-      <span className="float-right p-1">
-        <Search />
-      </span>
-    </Menu>
+    //   {user && (
+    //     <SubMenu
+    //       key="sub-menu"
+    //       icon={<SettingOutlined />}
+    //       // title={user.email && user.email.split("@")[0]}
+    //       title={user.email && user.email.split("@")[0]}
+    //     >
+    //       {user && user.role === "lead" && (
+    //         <Item>
+    //           <Link to="/user/history">Dashboard</Link>
+    //         </Item>
+    //       )}
+    //       {user && user.role === "admin" && (
+    //         <Item>
+    //           <Link to="/admin/dashboard">Dashboard</Link>
+    //         </Item>
+    //       )}
+    //       <Item icon={<LogoutOutlined />} onClick={logout}>
+    //         Logout
+    //       </Item>
+    //     </SubMenu>
+    //   )}
+    //   <span className="float-right p-1">
+    //     <Search />
+    //   </span>
+    // </Menu>
   );
 };
 
